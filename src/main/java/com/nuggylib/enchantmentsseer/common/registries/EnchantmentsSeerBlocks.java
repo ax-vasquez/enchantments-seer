de2a5b62df8823e5213b54dcc4304d267f27c4f9
Armando Vasquez <mando@Armandos-MacBook-Pro.local>
2021-08-31 22:27:39 -0500

    Seers stone fix and code refactor (#21)

--- src/main/java/com/nuggylib/enchantmentsseer/common/registries/EnchantmentsSeerBlocks.java
+++ src/main/java/com/nuggylib/enchantmentsseer/common/registries/EnchantmentsSeerBlocks.java
@@ -1,22 +1,13 @@
 package com.nuggylib.enchantmentsseer.common.registries;
 
 import com.nuggylib.enchantmentsseer.EnchantmentsSeer;
-import com.nuggylib.enchantmentsseer.common.block.basic.BlockResource;
 import com.nuggylib.enchantmentsseer.common.block.prefab.BlockTile;
-import com.nuggylib.enchantmentsseer.common.content.BlockType;
 import com.nuggylib.enchantmentsseer.common.content.BlockTypeTile;
 import com.nuggylib.enchantmentsseer.common.item.block.ItemBlockEnchantmentsSeer;
-import com.nuggylib.enchantmentsseer.common.item.block.ItemBlockResource;
 import com.nuggylib.enchantmentsseer.common.item.block.ItemBlockSeersEnchantingTable;
 import com.nuggylib.enchantmentsseer.common.registration.impl.BlockDeferredRegister;
 import com.nuggylib.enchantmentsseer.common.registration.impl.BlockRegistryObject;
-import com.nuggylib.enchantmentsseer.common.resource.BlockResourceInfo;
-import com.nuggylib.enchantmentsseer.common.resource.PrimaryResource;
 import com.nuggylib.enchantmentsseer.common.tile.block.TileEntitySeersEnchantmentTable;
-import com.nuggylib.enchantmentsseer.common.util.EnumUtils;
-
-import java.util.LinkedHashMap;
-import java.util.Map;
 
 /**
  * Block registy class
@@ -32,23 +23,7 @@ public class EnchantmentsSeerBlocks {
 
     public static final BlockDeferredRegister BLOCKS = new BlockDeferredRegister(EnchantmentsSeer.MOD_ID);
 
-    public static final Map<PrimaryResource, BlockRegistryObject<?, ?>> PROCESSED_RESOURCE_BLOCKS = new LinkedHashMap<>();
-
-    static {
-        // resource blocks
-        for (PrimaryResource resource : EnumUtils.PRIMARY_RESOURCES) {
-            if (resource.getResourceBlockInfo() != null) {
-                PROCESSED_RESOURCE_BLOCKS.put(resource, registerResourceBlock(resource.getResourceBlockInfo()));
-            }
-        }
-    }
-
     // Blocks
-    public static final BlockRegistryObject<BlockTile<TileEntitySeersEnchantmentTable, BlockTypeTile<TileEntitySeersEnchantmentTable>>, ItemBlockEnchantmentsSeer> SEERS_ENCHANTING_TABLE = BLOCKS.register("fuelwood_heater", () -> new BlockTile<>(EnchantmentsSeerBlockTypes.SEERS_ENCHANTING_TABLE), ItemBlockSeersEnchantingTable::new);
+    public static final BlockRegistryObject<BlockTile<TileEntitySeersEnchantmentTable, BlockTypeTile<TileEntitySeersEnchantmentTable>>, ItemBlockEnchantmentsSeer> SEERS_ENCHANTING_TABLE = BLOCKS.register("seers_enchanting_table", () -> new BlockTile<>(EnchantmentsSeerBlockTypes.SEERS_ENCHANTING_TABLE), ItemBlockSeersEnchantingTable::new);
 
-    private static BlockRegistryObject<BlockResource, ItemBlockResource> registerResourceBlock(BlockResourceInfo resource) {
-        return BLOCKS.registerDefaultProperties("block_" + resource.getRegistrySuffix(), () -> new BlockResource(resource), (block, properties) -> {
-            return new ItemBlockResource(block, properties);
-        });
-    }
 }
