dd6ea1912866ef2a652b84ec6cceedd9ffb0e9ea
Larah Armstrong <armstrong.llarah@gmail.com>
2021-09-06 17:02:37 -0500

    Implement Uncharged Seer's Stone (#33)
    * Implement Uncharged Seer's Stone :peach: (#33)

--- src/main/java/com/nuggylib/enchantmentsseer/common/registries/EnchantmentsSeerItems.java
+++ src/main/java/com/nuggylib/enchantmentsseer/common/registries/EnchantmentsSeerItems.java
@@ -5,6 +5,7 @@ import com.nuggylib.enchantmentsseer.common.item.ItemSeersEnchantedPage;
 
 import com.nuggylib.enchantmentsseer.common.item.ItemSeersManuscript;
 import com.nuggylib.enchantmentsseer.common.item.ItemSeersStone;
+import com.nuggylib.enchantmentsseer.common.item.ItemSeersStoneU;
 import com.nuggylib.enchantmentsseer.common.registration.impl.ItemDeferredRegister;
 import com.nuggylib.enchantmentsseer.common.registration.impl.ItemRegistryObject;
 import net.minecraft.item.Item;
@@ -20,6 +21,7 @@ public class EnchantmentsSeerItems {
 
     public static final ItemDeferredRegister ITEMS = new ItemDeferredRegister(EnchantmentsSeer.MOD_ID);
 
+    public static final ItemRegistryObject<Item> SEERS_STONE_U = ITEMS.register("seers_stone_u", ItemSeersStoneU::new);
     public static final ItemRegistryObject<Item> SEERS_STONE = ITEMS.register("seers_stone", ItemSeersStone::new);
     public static final ItemRegistryObject<Item> SEERS_MANUSCRIPT = ITEMS.register("seers_manuscript", ItemSeersManuscript::new);
     public static final ItemRegistryObject<Item> SEERS_ENCHANTED_PAGE = ITEMS.register("seers_enchanted_page", ItemSeersEnchantedPage::new);
